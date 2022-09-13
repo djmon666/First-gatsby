3341aba2bdd4d2905eebb68863fd482470adc515
djmon666 <djmonman@gmail.com>
2022-09-13 12:15:56 +0200

    Go

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   )
